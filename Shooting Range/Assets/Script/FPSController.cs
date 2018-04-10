083ae71c762f0a5274e8858550da9336ad0b2b69
JoaoPBakaJun <joaobakajun@hotmail.com>
2018-04-10 14:30:07 +0100

    Initial commit

---
+++ Shooting Range/Assets/Script/FPSController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(CharacterController))]
+public class FPSController : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;
+	public float mouseSensitivity = 2.0f;
+	float verticalRotation = 0;
+	public float jumpHight = 5.0f;
+	public float verticalCameraLimit = 60.0f;
+
+	float verticalVelocity = 0;
+
+	CharacterController characterController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		characterController = GetComponent<CharacterController> ();
+
+		Screen.lockCursor = true;
+		//Cursor.lockState
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		//Rotation with mouse
+		float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+		transform.Rotate (0, horizontalRotation, 0);
+
+		verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		verticalRotation = Mathf.Clamp (verticalRotation, -verticalCameraLimit, verticalCameraLimit);
+
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
+
+
+		//Movement
+		float verticalSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float horizontalSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+		if (characterController.isGrounded && Input.GetButtonDown ("Jump"))
+		{
+			verticalVelocity = jumpHight;
+		}
+
+		Vector3 speed = new Vector3 (horizontalSpeed, verticalVelocity, verticalSpeed);
+
+		speed = transform.rotation * speed;
+
+		characterController.Move (speed * Time.deltaTime);
+
+	}
+
+
+
+}
---
+++ Shooting Range/Assets/Script/Shooting.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+	public GameObject bullet_prefab;
+	public GameObject bullet2_prefab;
+	float bulletForce = 100f;
+	public float bulletRange = 100.0f;
+	public float cooldown = 0.5f;
+	float cooldownRemaining = 0;
+	bool weaponSwap = false;
+
+	public GameObject debrisPrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+
+	// to change weapen prefab make 2 gameobject prefab things and if key press 
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		cooldownRemaining -= Time.deltaTime;
+
+		if (Input.GetButtonDown (e)) 
+		{
+			weaponSwap = true;
+		}
+
+		if (weaponSwap == false) {
+			if (Input.GetButtonDown ("Fire1")) {
+
+				Camera camera = Camera.main;
+				GameObject bullet = (GameObject)Instantiate (bullet_prefab, camera.transform.position + camera.transform.forward, camera.transform.rotation);
+				bullet.GetComponent<Rigidbody> ().AddForce (camera.transform.forward * bulletForce, ForceMode.Impulse);
+
+			}	
+		} 
+		else if (weaponSwap == false)
+		{
+			if (Input.GetButtonDown ("Fire1")) {
+
+				Camera camera = Camera.main;
+				GameObject bullet = (GameObject)Instantiate (bullet2_prefab, camera.transform.position + camera.transform.forward, camera.transform.rotation);
+				bullet.GetComponent<Rigidbody> ().AddForce (camera.transform.forward * bulletForce, ForceMode.Impulse);
+
+			}	
+		}
+
+
+
+
+
+		/*if (Input.GetButtonDown ("Fire1"))
+		{
+
+			Camera camera = Camera.main;
+			GameObject bullet = (GameObject)Instantiate (bullet_prefab, camera.transform.position + camera.transform.forward, camera.transform.rotation);
+			bullet.GetComponent<Rigidbody> ().AddForce (camera.transform.forward * bulletForce, ForceMode.Impulse);
+
+		}*/
+
+		/*if (Input.GetButton("Fire1") && cooldownRemaining <= 0) 
+		{
+			cooldownRemaining = cooldown;
+			Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
+			RaycastHit hitInfo;
+
+			if (Physics.Raycast (ray, out hitInfo, bulletRange)) 
+			{
+				Vector3 hitPoint = hitInfo.point;
+				GameObject target = hitInfo.collider.gameObject;
+
+				Debug.Log ("Hit Object: " + target.name);
+				Debug.Log ("Hit Point: " + hitPoint);
+
+
+
+				if (debrisPrefab != null) 
+				{
+					Instantiate (debrisPrefab, hitPoint, Quaternion.identity);
+				}
+
+
+			}
+		}*/
+
+	}
+}
